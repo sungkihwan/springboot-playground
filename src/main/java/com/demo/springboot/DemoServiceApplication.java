c08d96e1b2a8686998615117af9dc9a6d8f9ea43
kihwan <skh@companylabs.co.kr>
2023-08-20 10:45:06 +0900

    naming 수정

--- src/main/java/com/demo/springboot/DemoServiceApplication.java
+++ src/main/java/com/demo/springboot/DemoServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmSscmServiceApplication {
+public class DemoServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SmSscmServiceApplication.class, args);
+        SpringApplication.run(DemoServiceApplication.class, args);
     }
 
 }
